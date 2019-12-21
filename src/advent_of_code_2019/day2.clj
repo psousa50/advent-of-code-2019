176645db0d0cd52f569ba0551a26b82b2790b684
Pedro Sousa <pedronsousa@gmail.com>
2019-12-21 00:22:15 +0000

    Refacto to use micro code and implement part2

--- src/advent_of_code_2019/day2.clj
+++ src/advent_of_code_2019/day2.clj
@@ -1,7 +1,7 @@
-(ns advent-of-code-2019.day2
-  (:require [advent-of-code-2019.core :as core]))
+`(ns advent-of-code-2019.day2
+   (:require [advent-of-code-2019.core :as core]))
 
-(defn- get-indirect-input
+(defn- get-input-indirect
   [{program :program} position]
   (nth program (nth program position)))
 
@@ -9,55 +9,80 @@
   [{program :program} position]
   (nth program position))
 
-(defn- add
-  [{pc :pc :as state}]
-  (+ (get-indirect-input state (+ pc 1)) (get-indirect-input state (+ pc 2))))
-
-(defn- multiply
-  [{pc :pc :as state}]
-  (* (get-indirect-input state (+ pc 1)) (get-indirect-input state (+ pc 2))))
-
-(defn- inc-pc
-  [{pc :pc} inc]
-  (+ pc inc))
-
 (defn- get-op-code
   [{pc :pc program :program}]
   (nth program pc))
 
-(defn- new-state
-  [{program :program} pc position new-value]
-  {:program (assoc program position new-value) :pc pc})
+(defn- pop2
+  [stack]
+  (pop (pop stack)))
+
+(defn- fetch-indirect
+  [{pc :pc stack :stack :as state}]
+  (-> state
+      (update :pc inc')
+      (assoc :stack (conj stack (get-input-indirect state pc)))))
+
+(defn- fetch-direct
+  [{pc :pc stack :stack :as state}]
+  (-> state
+      (update :pc inc')
+      (assoc :stack (conj stack (get-input state pc)))))
+
+(defn- sum
+  [{stack :stack :as state}]
+  (assoc state :stack (conj (pop2 stack) (+ (first stack) (second stack)))))
+
+(defn- multiply
+  [{stack :stack :as state}]
+  (assoc state :stack (conj (pop2 stack) (* (first stack) (second stack)))))
+
+(defn- store
+  [{stack :stack program :program :as state}]
+  (-> state
+      (assoc :stack (conj (pop2 stack)))
+      (assoc :program (assoc program (first stack) (second stack)))))
 
 (defn- micro
-  [state & ops]
-(loop [stack '() ops#: ops]
-  (case (first (keys ops))
-  :fetch-indirect (recur (conj stack (get-indirect-input state (pop stack))) next ops#))))
-  :+ (+ )
+  [state & micro-op]
+  (loop [state# state micro-op# micro-op]
+    (if micro-op#
+      (recur ((first micro-op#) state#) (next micro-op#))
+      state#)))
+
 
 (defn- run-op
   [op-code state]
   (case op-code
-    1 (micro :fetch-indirect  :fetch-indirect :sum :fetch-direct :store)
-    1 (new-state state (inc-pc state 4) (get-input state (+ (:pc state) 3)) (add state))
-    2 (new-state state (inc-pc state 4) (get-input state (+ (:pc state) 3)) (multiply state))))
+    1 (micro state fetch-indirect fetch-indirect sum fetch-direct store)
+    2 (micro state fetch-indirect fetch-indirect multiply fetch-direct store)))
 
 (defn intcode-computer
   [program]
-  (loop [state {:program program :pc 0}]
+  (loop [state {:program program :pc 0 :stack '()}]
     (let [op-code (get-op-code state)]
       (if (= op-code 99)
         state
-        (recur (run-op op-code state))))))
+        (recur (run-op op-code (update state :pc inc)))))))
 
 
 (def program [1 0 0 3 1 1 2 3 1 3 4 3 1 5 0 3 2 1 6 19 1 9 19 23 2 23 10 27 1 27 5 31 1 31 6 35 1 6 35 39 2 39 13 43 1 9 43 47 2 9 47 51 1 51 6 55 2 55 10 59 1 59 5 63 2 10 63 67 2 9 67 71 1 71 5 75 2 10 75 79 1 79 6 83 2 10 83 87 1 5 87 91 2 9 91 95 1 95 5 99 1 99 2 103 1 103 13 0 99 2 14 0 0])
 ; (def program [1 0 0 3 99])
 
-(defn main
-  []
-  prn
+(defn get-program-output
+  [v1 v2]
   (nth (:program (intcode-computer
-                  (assoc (assoc program 1 12) 2 2))) 0))
+                  (assoc (assoc program 1 v1) 2 v2))) 0))
+
+(defn part1
+  []
+  (get-program-output 12 2))
+
+(defn part2
+  []
+  (for [noun (range 100) verb (range 100) :when (= (get-program-output noun verb) 19690720)]
+    (list noun verb)))
+
+(comment
+  (intcode-computer [1 0 0 3 99]))
 
